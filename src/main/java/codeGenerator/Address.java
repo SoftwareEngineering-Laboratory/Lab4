afaa212429b4ac04f01fac59f2d9e76a8b56293b
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-08-11 19:15:34 +0330

    Substitue Algorithm in Address class codeGenerator package
    Use Hashmap instead of switch case on variable

--- src/main/java/codeGenerator/Address.java
+++ src/main/java/codeGenerator/Address.java
@@ -1,5 +1,8 @@
 package codeGenerator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mohammad hosein on 6/28/2015.
  */
@@ -8,28 +11,36 @@ public class Address {
     public int num;
     public TypeAddress Type;
     public varType varType;
+    private Map<TypeAddress, String> typeToString;
 
     public Address(int num, varType varType, TypeAddress Type) {
         this.num = num;
         this.Type = Type;
         this.varType = varType;
+
+        this.generateTypeToStringMap();
+    }
+
+    private void generateTypeToStringMap() {
+        typeToString = new HashMap<>();
+        typeToString.put(TypeAddress.Direct, String.valueOf(this.num));
+        typeToString.put(TypeAddress.Indirect, "@" + this.num);
+        typeToString.put(TypeAddress.Imidiate, "#" + this.num);
+    }
+
+    private Map<TypeAddress, String> getTypeToStringMap() {
+        return this.typeToString;
     }
 
     public Address(int num, varType varType) {
         this.num = num;
         this.Type = TypeAddress.Direct;
         this.varType = varType;
+
+        this.generateTypeToStringMap();
     }
 
     public String toString() {
-        switch (Type) {
-            case Direct:
-                return num + "";
-            case Indirect:
-                return "@" + num;
-            case Imidiate:
-                return "#" + num;
-        }
-        return num + "";
+        return this.getTypeToStringMap().get(this.Type);
     }
 }
